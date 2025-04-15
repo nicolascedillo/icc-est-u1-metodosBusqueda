174ca3556c48a791ad71703a9bfc957e6dfb15b8
Nicolas <ecedilloa@est.ups.edu.ec>
2025-04-15 07:36:28 -0500

    Arreglo condición

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -5,7 +5,7 @@ public class MetodosBusqueda {
     private int[] arreglo;
 
     public int busquedaLineal (int[] arreglo, int x){
-        for (int i = 0;i<=arreglo.length;i++){
+        for (int i = 0; i < arreglo.length ; i++){
             if (arreglo[i] == x){
                 return i;
             }
